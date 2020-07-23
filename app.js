3fa3708dc23c4ca5bcedc2e7549b4f892b3f9eab
Bader Fahoum <fahoumbader@gmail.com>
2020-07-23 12:05:47 +0300

    final version

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// testing function
+// ------------------------------ testing function
 function test(name, testFunction) {
   function equal(x, y, message = `Expected ${x} to equal ${y}`) {
     if (x === y) {
@@ -33,46 +33,63 @@ function taskCompleter(htmlel) {
   console.log(htmlel);
   return `${htmlel.classList.toggle("striked")}`;
 }
-/// todoList logic
-
+// ------------------------------ The todoList logic
+// our selectors
 const addForm = document.querySelector(".add");
 const ullist = document.querySelector(".todos");
+const search = document.querySelector(".search input");
+
 // const delete = document.querySelector(".delete");
 
 console.log(ullist);
-// li template generator
-const generateTemplate = (todo) => {
+// ------------------------------  the template generator, which creates a different markup for a MAIN LI which could contain other LIs and differentiate with a SUB LI - with does not
+const generateTemplate = (todo, elementToAdd, targetEl = ``) => {
+  //create the li
   let taskli = document.createElement("li");
   taskli.classList.add("text-center");
-
+  // format the markup
   let spanli = document.createElement("span");
   spanli.classList.add("todotext");
   spanli.textContent = `${todo}`;
+
   taskli.appendChild(spanli);
+  // this is all the elements all li should contain
+  let divGrouper = document.createElement("div");
+  divGrouper.classList.add("igroup");
+  let trash = document.createElement("i");
+  let plus = document.createElement("i");
+  trash.classList.add("fa", "fa-trash", "delete");
+  plus.classList.add("fa", "fa-plus", "plus");
+  divGrouper.appendChild(trash);
+  taskli.appendChild(divGrouper);
+  // console.log(taskli);
+  // if its a mainn LI the flag should be with ul
+  if (elementToAdd === "ul") {
+    taskli.classList.add("main");
+    taskli.lastChild.prepend(plus);
+    ullist.appendChild(taskli);
+  }
+  /// a sub li element
+  if (elementToAdd === "li") {
+    taskli.classList.add("subli");
+    taskli.querySelector("span").classList.add("insideli");
+    targetEl.parentElement.parentElement
+      .querySelector("span")
+      .appendChild(taskli);
+  }
 
-  let icon = document.createElement("i");
-  icon.classList.add("fa", "fa-trash", "delete");
-  taskli.appendChild(icon);
-  console.log(taskli);
-  // const html = `
-  //    <li class="text-center">
-  //         <span>${todo}</span>
-  //         <i class="fa fa-trash delete" aria-hidden="true"></i>
-  //       </li>
-  //   `;
-  // ullist.innerHTML += html;
-
-  ullist.appendChild(taskli);
+  return taskli;
 };
-
+// the form selector
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const templateFlag = "ul";
   // console.log(addForm.add.value);
   // const li = document.createElement('li');
   let todo = addForm.add.value.trim();
   // ullist.prepend(li);
   if (todo.length) {
-    generateTemplate(todo);
+    generateTemplate(todo, templateFlag);
     addForm.reset();
   } else {
     alert("please enter something");
@@ -83,10 +100,61 @@ addForm.addEventListener("submit", (e) => {
 ullist.addEventListener("click", (e) => {
   // console.log(e.target);
   if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
+    e.target.parentElement.parentElement.remove();
   }
 
   if (e.target.classList.contains("todotext")) {
     taskCompleter(e.target);
   }
+
+  if (e.target.classList.contains("plus")) {
+    console.log(e.target.parentElement.parentElement);
+    let templateFlag = "li";
+    let todo = addForm.add.value.trim();
+    // ullist.prepend(li);
+    if (todo.length) {
+      generateTemplate(todo, templateFlag, e.target);
+      addForm.reset();
+    } else {
+      alert("please enter something");
+    }
+  }
+});
+
+//----------------------------- filter and search logic
+const filterTodos = (term) => {
+  //filter words that doesnt include the word, add a filtered class to them to remove them from the list
+  Array.from(ullist.children)
+    .filter((todoItem) => {
+      return !todoItem.textContent.toLowerCase().includes(term);
+      // console.log(todoItem.textContent);
+      // return true;
+    })
+    .forEach((todoItem) => {
+      todoItem.classList.add("filtered");
+    });
+  // // shorter syntax but less readable for newcomers:
+  // Array.from(ullist.children)
+  //     .filter((todoItem) => !todoItem.textContent.includes(term))
+  //     .forEach((todoItem) => todo.classList.add('filtered'));
+
+  // remove filtered when the word is in the text
+  Array.from(ullist.children)
+    .filter((todoItem) => {
+      return todoItem.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todoItem) => {
+      todoItem.classList.remove("filtered");
+    });
+
+  return Array.from(ullist.children).filter((todoItem) => {
+    return todoItem.textContent.toLowerCase().includes(term);
+  });
+};
+
+//keyup event - this will fire out search logic
+search.addEventListener("keyup", () => {
+  const term = search.value.trim().toLowerCase();
+
+  filterTodos(term);
 });
--- app.test.js
+++ app.test.js
@@ -3,11 +3,77 @@ test("This should test trimming all extra whitespaces", (t) => {
   t.equal(whiteSpaces(" hello world     "), "hello world");
 });
 
-test("This should test adding a strike though for a given text", (t) => {
-  t.equal(
-    taskCompleter(
-      '<span class="todotext">test for a ro7 eshtri baqdones</span>'
-    ),
-    '<span class="todotext striked">test for a ro7 eshtri baqdones</span>'
-  );
+test("this should test generating the right ul", (t) => {
+  // select the main list for manipulation
+
+  const ullist = document.querySelector("ul");
+  // get the number of li items before running add and delete
+  let expected = Array.from(ullist.children);
+
+  // add new task
+  let taskli = generateTemplate("newly added task", "ul");
+
+  // select the newly added task from the list by passing through all elements making sure we have the new element added
+  let results = Array.from(ullist.children);
+  // sized of the ul array should match, because we addded an item, made sure its present (by selecting it) and then deleting it
+
+  t.equal(results.length, expected.length + 1);
+});
+
+test("filtering an item with the search", (t) => {
+  const ul = document.querySelector("ul");
+  // console.log(xx)
+  const ularray = Array.from(ul.children);
+  // console.log(xx);
+  t.equal(ularray.toString(), Array.from(ul.children).toString());
+});
+
+// check if strikthrough works
+test("check for validite of strikethrough-ing tasks", (t) => {
+  // new Task
+  let taskli = generateTemplate("strikethrough task test", "ul");
+
+  const ullist = document.querySelector("ul");
+  // const litest = document.querySelector("li");
+
+  // this should mark the first
+  Array.from(ullist.children).forEach((e) => {
+    let spanli = e.querySelector(".todotext");
+    console.log(spanli.textContent);
+
+    if (spanli.textContent == "strikethrough task test") {
+      console.log("spanli is ==> ", e);
+      spanli.click();
+      t.equal(spanli.className, "todotext striked");
+      // t.notEqual(spanli.className, "Not strikeThrough");
+    }
+  });
+});
+
+// check if strikthrough works
+test("Delete li when trashbin is selected", (t) => {
+  // select the main list for manipulation
+  const ullist = document.querySelector("ul");
+  // get the number of li items before running add and delete
+  let expected = Array.from(ullist.children);
+
+  // add new task
+  let taskli = generateTemplate("delete task test", "ul");
+
+  // select the newly added task from the list by passing through all elements making sure we have the new element added
+  Array.from(ullist.children).forEach((e) => {
+    let spanli = e.querySelector(".todotext");
+    // check if its the wanted li
+    if (spanli.textContent == "delete task test") {
+      // console for debugging incase of an error
+      console.log("spanli is ==> ", e);
+      // select the trash( the button that deletes the li)
+      const trash = e.lastChild.lastChild;
+      //   delete the element after adding it
+      trash.click();
+    }
+  });
+  // sized of the ul array should match, because we addded an item, made sure its present (by selecting it) and then deleting it
+
+  t.equal(Array.from(ullist.children).length, expected.length);
 });
