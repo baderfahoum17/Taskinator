582241268f6a66f9efd3537baa7a3bf1dfe388a8
bader fahoum <fahoumbader@gmail.com>
2020-07-28 14:02:19 +0300

    Update missing.js

--- handlers/missing.js
+++ handlers/missing.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 function missingHandler(request, response) {
-  filePath = path.join(__dirname, "..", "notfound.html");
+  filePath = path.join(__dirname, "../public/", "notfound.html");
   fs.readFile(filePath, (err, notfoundfile) => {
     response.writeHead(404, {"content-type": "text/html"});
     response.end(notfoundfile);
